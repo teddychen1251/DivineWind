53f2391f1ec9af28df0dcaf966de3dd59affb190
Teddy Chen <teddychen1251@gmail.com>
2023-09-03 00:19:02 -0700

    Folder refactors

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 const CELL_HEIGHT = 4;
-const CELL_OUTER_ARC_THRESHOLD = 6;
-const INNER_RADIUS = 3;
-const LAYERS = 3;
+const CELL_OUTER_ARC_THRESHOLD = 5;
+const INNER_RADIUS = 2;
+const LAYERS = 5;
